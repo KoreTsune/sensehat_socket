179f2f13aac7d0e64cf332f3a111d9128eb036fa
ayuki_j <ajjt66554@gmail.com>
2019-12-07 17:12:26 +0900

    separate moisture.py & reformat cord

---
+++ moisture.py
@@ -0,0 +1,45 @@
+import datetime
+import time
+import json
+from pytz import timezone
+import serial
+import re
+import urllib.request
+
+
+def json_sensor_value(con, url):
+    moisture_data = re.findall(r'[0-9]+', con.readline().decode('utf-8'))
+    # moisture_dataでうまく値が取れなかった時の処理です．
+    if len(moisture_data) == 0:
+        moisture_data = ["0"]
+    now_time = datetime.datetime.now(timezone("Asia/Tokyo")).strftime("%Y%m%d %H:%M:%S")
+
+    # 送るdict(json)データです．
+    send_mes = {'datetime': now_time, 'moisture': moisture_data[0]}
+
+    # requests post
+    headers = {'Content-Type': 'application/json', }
+    req = urllib.request.Request(url, json.dumps(send_mes).encode(), headers)
+    with urllib.request.urlopen(req) as res:
+        body = res.read()
+
+    return now_time, moisture_data[0]
+
+
+def main():
+    url = ""
+    con = serial.Serial('/dev/ttyACM0', 9600)
+    time.sleep(5)
+
+    while True:
+        now_time, moisture_data = json_sensor_value(con, url)
+
+        print(now_time)
+        print("Moisture   : {0}".format(moisture_data))
+
+        print("\n")
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
